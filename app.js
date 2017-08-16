791c4bb4ba88701b41dd1ffabc4c94fe33b931ba
Han <leook0209@gmail.com>
2017-08-16 13:21:48 +0800

    Add echo function

--- app.js
+++ app.js
@@ -3,10 +3,6 @@
 const token = process.env.FB_PAGE_ACCESS_TOKEN;
 const vtoken = process.env.FB_VERIFY_ACCESS_TOKEN;
 
-// const config = require('./config.js');
-// const token = config.FB_PAGE_ACCESS_TOKEN;
-// const vtoken = config.FB_VERIFY_ACCESS_TOKEN;
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
@@ -34,6 +30,97 @@ app.get('/webhook/', function (req, res) {
 	}
 	res.send('Error, wrong token');
 })
+
+app.post('/webhook', function (req, res) {
+    var data = req.body;
+  
+    // Make sure this is a page subscription
+    if (data.object === 'page') {
+  
+      // Iterate over each entry - there may be multiple if batched
+      data.entry.forEach(function(entry) {
+        var pageID = entry.id;
+        var timeOfEvent = entry.time;
+  
+        // Iterate over each messaging event
+        entry.messaging.forEach(function(event) {
+          if (event.message) {
+            receivedMessage(event);
+          } else {
+            console.log("Webhook received unknown event: ", event);
+          }
+        });
+      });
+
+      res.sendStatus(200);
+    }
+});
+    
+function receivedMessage(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+  
+    console.log("Received message for user %d and page %d at %d with message:", 
+      senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+  
+    var messageId = message.mid;
+  
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+  
+    if (messageText) {
+  
+      switch (messageText) {
+        case 'generic':
+        //   sendGenericMessage(senderID);
+          break;
+  
+        default:
+          sendTextMessage(senderID, messageText);
+      }
+    } else if (messageAttachments) {
+      sendTextMessage(senderID, "Message with attachment received");
+    }
+}
+
+function sendTextMessage(recipientId, messageText) {
+    var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        text: messageText
+      }
+    };
+  
+    callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+    request({
+      uri: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: { access_token: token },
+      method: 'POST',
+      json: messageData
+  
+    }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var recipientId = body.recipient_id;
+        var messageId = body.message_id;
+  
+        console.log("Successfully sent generic message with id %s to recipient %s", 
+          messageId, recipientId);
+      } else {
+        console.error("Unable to send message.");
+        console.error(response);
+        console.error(error);
+      }
+    });  
+}
+
 
 // Spin up the server
 app.listen(app.get('port'), function() {
