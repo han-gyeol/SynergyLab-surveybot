8d8ce653a068af4c7c6f88d0a9198d86631d156d
Han <leook0209@gmail.com>
2017-08-16 12:24:35 +0800

    Show config

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    /**
+     * facebook message hub.verify_token
+     */
+    verify_token: "all-your-base-are-belong-to-me",
+
+    /**
+     * listening port number
+     */
+    port: 5000
+};
