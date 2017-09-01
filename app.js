cefb100a5523e771d3b87b3f917434f850ee962d
Han <leook0209@gmail.com>
2017-09-01 18:22:08 +0800

    change url

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ server.get('/', function(req, res) {
 
 // Listen for facebook webhook
 server.get('/api/messages', function(req, res) {
+    console.log("Received token: " + req.query['hub.verify_token']);
+    console.log("Saved token: " + process.env.FB_VERIFY_ACCESS_TOKEN);
     if (req.query['hub.verify_token'] === process.env.FB_VERIFY_ACCESS_TOKEN) {
     	res.send(req.query['hub.challenge'])
     }
